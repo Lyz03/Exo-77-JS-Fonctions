f893b8d0ab474e07a3714c1a12629dc6c7b1b814
Lyz03 <lizoe@netcourrier.com>
2021-11-18 12:20:08 +0100

    part 4

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,10 +1,12 @@
 /* Ecrire ci dessous une fonction permettant d'ajouter du texte dans le div appelé container */
 
-
+function text() {
+    document.getElementById('container').innerText = "nouveau texte"
+}
 
 /* Appeler la fonction */
 
-
+text();
 
 
 
